6d0d12ab597b75df66cfb251bef4f2b27f26db80
Kenzie Togami <ket1999@gmail.com>
2013-09-21 09:32:41 -0700

    Changed Point3D to have non-auto registers.
    Cleaner.

--- src/k/pocm/points/Point3D.java
+++ src/k/pocm/points/Point3D.java
@@ -7,23 +7,18 @@ public abstract class Point3D {
 
 	private static final HashMap<Class<? extends Point3D>, PointType> types = new HashMap<Class<? extends Point3D>, PointType>();
 
-	public Point3D(String id) {
-		if(classStorage.get(id) == getClass()) {
-			return;
-		}
-		if (classStorage.containsKey(id)) {
-			System.err.println("An point id is being overwritten! ID:" + id
-					+ ", Class:" + getClass().getName());
-		}
-		classStorage.put(id, getClass());
-	}
-
 	public static Class<? extends Point3D> matchingClassForID(String id) {
 		return classStorage.get(id);
 	}
 
 	public static PointType getTypeForClass(Class<? extends Point3D> pointClass) {
 		return types.get(pointClass);
+	}
+
+	public static void registerPointWithType(String id,
+			Class<? extends Point3D> clazz, PointType type) {
+		classStorage.put(id, clazz);
+		types.put(clazz, type);
 	}
 
 }
